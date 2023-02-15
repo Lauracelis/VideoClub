60a2aad9a6cd3fff0033997c44cda728b8652473
NGCnathalia <nthaliagarcia@gmail.com>
2023-02-15 14:47:32 -0500

    Create NavBar and Create Movie

--- frontend/frontend_/src/App.jsx
+++ frontend/frontend_/src/App.jsx
@@ -4,19 +4,38 @@ import './App.css'
 import MovieList from './components/MovieList'
 import CreateMovie from './components/CreateMovie'
 import EditMovie from './components/EditMovie'
-import {BrowserRouter, Routes, Route} from 'react-router-dom'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <div className="App">
-      <h1>VIDEOCLUB</h1>
+      
+      <nav className="navbar navbar-expand-lg bg-body-tertiary">
+        <div className="container">
+          <a className="navbar-brand" href="#">VIDEOCLUB</a>
+          <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+              <li className="nav-item">
+                <a className="nav-link active" aria-current="page" href="#">Inicio</a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="createMovie">Agregar Pelicula</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+
       <BrowserRouter>
         <Routes>
-          <Route path='/' element ={<MovieList />} exact></Route>
-          <Route path='/createMovie' element ={<CreateMovie />} exact></Route>
-          <Route path='/editMovie' element ={<EditMovie />} exact></Route>
+          <Route path='/' element={<MovieList />} exact></Route>
+          <Route path='/createMovie' element={<CreateMovie />} exact></Route>
+          <Route path='/editMovie' element={<EditMovie />} exact></Route>
         </Routes>
       </BrowserRouter>
 
--- frontend/frontend_/src/components/CreateMovie.jsx
+++ frontend/frontend_/src/components/CreateMovie.jsx
@@ -1,9 +1,50 @@
-import React from 'react'
+import React, { useState } from 'react'
+import uniquid from 'uniquid'
 
 export const CreateMovie = () => {
+      //Hooks
+      const[nombre,setNombre]=useState('')
+      const[email,setEmail]=useState('')
+      const[telefono,setTelefono]=useState('')
+
+    function createMovie(){
+      var usuario = {
+        nombre:nombre,
+        email:email,
+        telefono: telefono,
+        idusuario: uniquid()
+      }
+      console.log(usuario)
+
+      axios.post('/api/usuario/agregarusuario', usuario)
+      .then(res=>{
+        alert(res.data)
+      })
+    }
+
     return (
-        <div>
-            <h1>CreateMovie</h1>
+        <div className='container'>
+            <div className='row'>
+                    <h1>CreateMovie</h1>
+            </div>
+            <div className='row'>
+                <div className='col-sm-6 offset-3'>
+                    <div className='mb-3'>
+                        <label htmlFor="nombre" className='form-label'>Nombre</label>
+                        <input type="text" className='form-control' value={nombre} onChange={(e)=> {setNombre(e.target.value)}}></input>
+                    </div>
+                    <div className='mb-3'>
+                        <label htmlFor="email" className='form-label'>Email</label>
+                        <input type="text" className='form-control'value={email} onChange={(e)=> {setEmail(e.target.value)}}></input>
+                    </div>
+                    <div className='mb-3'>
+                        <label htmlFor="telefono" className='form-label'>Telefono</label>
+                        <input type="text" className='form-control'value={nombre} onChange={(e)=> {setNombre(e.target.value)}}></input>
+                    </div>
+                    <button onClick={createMovie} className="btn btn-success">Guardar</button>
+                </div>
+            </div>
+
         </div>
     )
 }
